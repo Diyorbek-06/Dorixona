9c698ee7f63908133ea174b4ad3f8bc5113bc5f8
diyor-06 <sizning-email@gmail.com>
2025-03-14 02:20:15 +0500

    profil qo'shildi

--- account/forms.py
+++ account/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django import forms
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import AuthenticationForm
-
+from .models import Profile
 
 class RegisterForm(forms.ModelForm):
     confirm_password = forms.CharField(max_length=200, required=True, widget=forms.PasswordInput())
@@ -34,3 +34,18 @@ class LoginForm(AuthenticationForm):
         if not User.objects.filter(username=username).exists():
             raise ValidationError('Bunday username yoki password topilmadi!')
         return username
+
+
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
+
